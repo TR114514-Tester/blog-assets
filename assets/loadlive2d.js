900d97dd92690563adb6b80d35a2ae57236ac330
一位旅行者114514 <2197077248@qq.com>
2025-12-19 08:47:21 +0000

    Add files via upload

---
+++ assets/loadlive2d.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://blog.traveler.dpdns.org/live2d/autoload.js';
+document.body.appendChild(script);
