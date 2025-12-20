1fcc79705f02b872d58fa10089df01b820a3aac4
一位旅行者114514 <2197077248@qq.com>
2025-12-20 18:26:08 +0800

    Delete assets/loadlive2d.js

--- assets/loadlive2d.js
+++
@@ -1,4 +0,0 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://blog.traveler.dpdns.org/live2d/autoload.js';
-document.body.appendChild(script);
